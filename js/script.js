c651c5fd17f443e631ff1a377d3e055fd2d839ee
Gerardo Antonio Ruelas Hernández <gahdz14@gmail.com>
2024-07-02 21:21:43 -0600

    Add: Nuevo Proyecto, App Salón

--- js/script.js
+++ js/script.js
@@ -1,58 +1,61 @@
-
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
     iniciarApp();
-})
+});
 
 function iniciarApp() {
-    // cambiarTexto();
     setInterval(cambiarTexto, 2000);
     mostrarOcultarMenu();
     seleccionar();
     scrollNav();
 }
+
 let textoIndex = 0; // Declarar e inicializar textoIndex fuera de la función cambiarTexto
 function cambiarTexto() {
-    let textos = ['Desarrollador Web', 'Programador', 'Ingeniero en Comunicaciones y Electrónica'];
+    const textos = ['Desarrollador Web', 'Programador', 'Ingeniero en Comunicaciones y Electrónica'];
+    const elemento = document.getElementById('titulo');
 
-    let elemento = document.getElementById('titulo');
     elemento.style.opacity = 0;
 
-    setTimeout(function(){
-        elemento.innerHTML = textos[textoIndex]; // Usar textoIndex para seleccionar el texto
+    setTimeout(function() {
+        elemento.innerHTML = textos[textoIndex];
         elemento.style.opacity = 1;
-        textoIndex = (textoIndex + 1) % textos.length; // Actualizar textoIndex para el siguiente texto
-    }, 800);     
+        textoIndex = (textoIndex + 1) % textos.length;
+    }, 800);
 }
 
 let menuVisible = false;
 //Función que oculta o muestra el menú
-function mostrarOcultarMenu(){
+function mostrarOcultarMenu() {
+    const nav = document.getElementById("nav");
     
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
-        menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
-        menuVisible = true;
+    if(menuVisible) {
+        nav.classList.remove("responsive");
+    } else {
+        nav.classList.add("responsive");
     }
+
+    menuVisible = !menuVisible;
 }
 
-function seleccionar(){
-    //oculto el menu una vez que selecciono una opción
-    document.getElementById("nav").classList = "";
-    menuVisible = false;
+function seleccionar() {
+    const enlaces = document.querySelectorAll('#nav a');
+    
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', () => {
+            document.getElementById("nav").classList.remove("responsive");
+            menuVisible = false;
+        });
+    });
 }
-
 
 function scrollNav() {
-    const enlaces = document.querySelectorAll('.responsive a')
-    enlaces.forEach( enlace => {
-        enlace.addEventListener('click', function(e){
+    const enlaces = document.querySelectorAll('.responsive a');
+    
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', function(e) {
             e.preventDefault();
-
-            const scroll = e.target.attributes.href.value;
-            const seccion = document.querySelector(scroll)
-            seccion.scrollIntoView({ behavior: "smooth"});
-        })
+            const seccion = document.querySelector(e.target.getAttribute('href'));
+            seccion.scrollIntoView({ behavior: "smooth" });
+        });
     });
 }
